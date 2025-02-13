ea3e1e27e7097e2a39986017d219bd6e42cba8d6
Gustavo Rocha <gustavorochagomessouto@gmail.com>
2025-02-13 11:35:45 -0300

    Resolvidos problemas com JS e CSS, Projeto Finalizado

--- app.js
+++ app.js
@@ -27,27 +27,27 @@ function criarMenu(disciplina) {
 const produtos = [
     {nome: 'Uniforme Home SPFC',
         descricao: 'Camiseta usada pelo São paulo futebol clube em partidas jogadas em casa', 
-        icon:'camisa1.jpg', 
+        icon:'camisa1.png', 
         cor:'black', 
         preco:'R$ 499,99', 
-        tamanhos: ['P', 'M', 'G', 'EG', 'EGG']},
+        tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2', 'GG3']},
     {nome: 'Uniforme Away SPFC', 
         descricao: 'Camiseta usada pelo São paulo futebol clube em partidas jogadas fora de casa',
         icon:'camisa2.jpg', 
         cor:'red', 
         preco: 'R$ 349,99',
-        tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2']},
+        tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2', 'GG3']},
     {nome: 
         'Uniforme Third SPFC', 
         descricao: 'Camiseta usada pelo São paulo futebol clube em partidas',
         icon:'camisa3.jpg', 
-        cor:'white', 
+        cor:'black', 
         preco: 'R$ 314,90',
-        tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2']},
+        tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2', 'GG3']},
     {nome: 
         'Jaqueta Tricolor 2025', 
         descricao: 'Jaqueta usada pelo São Paulo Futebol Clube em momentos do Pré-Jogo em 2025',
-        icon:'jaqueta1.jpg', 
+        icon:'jaqueta1.png', 
         cor:'grey', 
         preco: 'R$ 427,49',
         tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2', 'GG3']
@@ -55,7 +55,7 @@ const produtos = [
     {nome: 
         'Jaqueta Tricolor 2024', 
         descricao: 'Jaqueta usada pelo São Paulo Futebol Clube em momentos do Pré-Jogo em 2024',
-        icon:'jaqueta2.jpg', 
+        icon:'jaqueta2.png', 
         cor:'red', 
         preco: 'R$ 499,99',
         tamanhos: ['P', 'M', 'G', 'EG', 'EGG', 'GG2', 'GG3']
@@ -66,13 +66,51 @@ function criarMain(produto){
     const novoProduto = document.createElement('li')
     const novaFoto = document.createElement('img')
     const novoTitulo = document.createElement('h1')
+    const novaDesc = document.createElement('span')
+    const novoTamanho = document.createElement('ul')
+    const novoPreco = document.createElement('p')
+    const caixa = document.createElement('div')
+    const botaoComprar = document.createElement('button')
     const listar = document.getElementById('cards') 
+    const quadrados = document.createElement('div')
 
-    novoProduto.textContent = produto.nome
-    novaFoto.src = `./imgProdutos/$produto.disciplina.icon}`
+    quadrados.className = "quadrado"
+
+
+    novoTamanho.className = "tamanhos"
+
+    novoTitulo.textContent = produto.nome
+    novaFoto.src = `./imgProdutos/${produto.icon}`
+    novaDesc.textContent = produto.descricao
+    novoPreco.textContent = produto.preco
+    botaoComprar.textContent = "Adicionar ao Carrinho"
+    
+
+    caixa.appendChild(novoTitulo)
+    caixa.appendChild(novaDesc)
+    caixa.appendChild(novaFoto)
+    caixa.appendChild(novoPreco)
+    caixa.appendChild(novoTamanho)
+    caixa.appendChild(botaoComprar)
+
+    produto.tamanhos.forEach(function(tamanho){
+        const size = document.createElement("li")
+        size.textContent = tamanho
+        novoTamanho.appendChild(size)
+    })
+
+    novoProduto.appendChild(caixa)
+    quadrados.style = `--cor-background: ${produto.cor}`
+
+    quadrados.appendChild(novoProduto)
+
+
+
+    listar.appendChild(quadrados)
 
 
 }
 
 
 disciplinas.forEach(criarMenu)
+produtos.forEach(criarMain)
